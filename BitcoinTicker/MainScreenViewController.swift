f34e6925a91907364d8b920fc6864103ef3100bc
Jakub Suder <jakub.suder@gmail.com>
2014-11-15 13:22:32 +0100

    PriceController (no real price yet)

--- BitcoinTicker/MainScreenViewController.swift
+++ BitcoinTicker/MainScreenViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class MainScreenViewController: UIViewController {
 
+    @IBOutlet var priceLabel: UILabel!
+
+    let priceController = PriceController()
+    let formatter: NSNumberFormatter
+
+    required init(coder: NSCoder) {
+        formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        formatter.currencySymbol = "$"
+
+        super.init(coder: coder)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        showCurrentPrice()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func showCurrentPrice() {
+        if let price = priceController.currentPrice {
+            priceLabel.text = formatter.stringFromNumber(price)
+        } else {
+            priceLabel.text = "?"
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ BitcoinTicker/PriceController.swift
@@ -0,0 +1,13 @@
+//
+//  PriceController.swift
+//  BitcoinTicker
+//
+//  Created by Jakub Suder on 15/11/14.
+//  Copyright (c) 2014 Jakub Suder. All rights reserved.
+//
+
+import UIKit
+
+class PriceController : NSObject {
+    var currentPrice: Float?
+}
