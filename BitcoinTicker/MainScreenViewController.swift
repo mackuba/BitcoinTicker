6800200b8c6230fc614074db65ba0155e41a6ec8
Jakub Suder <jakub.suder@gmail.com>
2014-11-15 12:14:27 +0100

    screw storyboards

---
+++ BitcoinTicker/MainScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainScreenViewController.swift
+//  BitcoinTicker
+//
+//  Created by Jakub Suder on 15/11/14.
+//  Copyright (c) 2014 Jakub Suder. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
