53b22651291271c99cc6c3bec20a7477b48dc46a
Jakub Suder <jakub.suder@gmail.com>
2014-11-15 11:40:30 +0100

    initial commit

---
+++ BitcoinTickerTests/BitcoinTickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitcoinTickerTests.swift
+//  BitcoinTickerTests
+//
+//  Created by Jakub Suder on 15/11/14.
+//  Copyright (c) 2014 Jakub Suder. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BitcoinTickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
